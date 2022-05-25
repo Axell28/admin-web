3318d5b39b504eeb67827b9ad18621fbed61f461
xxSylarxx <85377044+xxSylarxx@users.noreply.github.com>
2022-05-25 17:45:41 -0500

    Update archivos.php
    Se corrige la duplicidad de extensión al momento de listar archivos

--- views/admin/archivos.php
+++ views/admin/archivos.php
@@ -348,8 +348,6 @@
                 limpiarNameFile(str) {
                     str = str.replace(/\s+/g, '-');
                     str = str.normalize("NFD").replace(/[\u0300-\u036f]/g, '');
-                    ext = str.split('.').pop();
-                    str = str.slice(0, 20) + '.' + ext;
                     return str;
                 }
             }
